63ad1d4f410dd75ded53b208f1d0e063c9379b64
Rishabh <rishabhbhatt956@gmail.com>
2025-05-24 04:50:12 +0530

    Add arrays.go with examples of zeroed values and array declarations

---
+++ 08_Arrays/arrays.go
@@ -0,0 +1,33 @@
+package main
+
+// Numberd sequence of specific length
+
+func main() {
+
+	// Zeroed values
+	// int -> 0, string -> "", bool -> false
+	// var nums [4]int
+
+	// nums[0] = 1
+
+	// fmt.Println(nums)
+
+	// array length
+	// fmt.Println(len(nums))
+
+	// var vals [4]bool
+	// fmt.Println(vals)
+
+	// var stg [4]string
+	// stg[0] = "lol"
+	// fmt.Println(stg)
+
+	// to declare it in single line
+	// nums := [3]int{1, 2, 3}
+	// fmt.Println(nums)
+
+	// 2d arrays
+	// nums := [2][2]int{{3, 4}, {5, 6}}
+	// fmt.Println(nums)
+
+}
